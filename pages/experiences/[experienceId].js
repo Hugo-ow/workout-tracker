af7f744dd786163fb929cd6ea6544134cad7eff0
Hugo-ow <owonahugo@gmail.com>
2026-06-08 10:46:11 +0200

    Refactor experienceId page to use Home component

--- pages/experiences/[experienceId].js
+++ pages/experiences/[experienceId].js
@@ -1,1 +1,6 @@
-export { default } from '../index'
+// pages/experiences/[experienceId].js
+import Home from '../index'
+
+export default function Experience(props) {
+  return <Home {...props} />
+}
